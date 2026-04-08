6c121f9dd930c7983b065f683f57f4735ce7a4a9
Avano <avanoberlingieri@gmail.com>
2026-04-08 14:10:10 -0400

    Created character model and api service

---
+++ src/app/services/service.ts
@@ -0,0 +1,22 @@
+import {Injectable, inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Character} from '../models/character';
+
+@Injectable({ providedIn: 'root' })
+export class Service {
+  private http = inject(HttpClient);
+  private apiUrl = 'https://hp-api.onrender.com/api';
+
+  getAllCharacters(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.apiUrl}/characters`);
+  }
+
+  getAllCharactersInHouse(house: string): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.apiUrl}/house/${house}`);
+  }
+
+  getCharacterById(id: string): Observable<Character> {
+    return this.http.get<Character>(`${this.apiUrl}/character/${id}`);
+  }
+}
